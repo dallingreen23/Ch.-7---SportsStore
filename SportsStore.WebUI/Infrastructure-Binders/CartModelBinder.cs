9394a558bce218852e269757665917c2002462ec
Lab <Lab@AIS-204LAB05>
2017-03-01 10:27:46 -0700

    git push

---
+++ SportsStore.WebUI/Infrastructure-Binders/CartModelBinder.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure_Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, CartModelBinderContext bindingContext)
+        {
+            CartModelBinder cart = null;
+            if (controllerContext.HttpContext.Session != null)
+            {
+                cart = (cart)controllerContext.HttpContext.Session[sessionKey];
+            }
+            if (cart == null)
+            {
+                cart = new Infrastructure_Binders.CartModelBinder();
+                if (controllerContext.HttpContext.Session != null)
+                {
+                    controllerContext.HttpContext.Session[sessionKey] = cart;
+                }
+            }
+            return cart;
+        }
+    }
+}
