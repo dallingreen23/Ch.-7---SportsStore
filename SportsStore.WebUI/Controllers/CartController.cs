ca16547c6b69c364ae53d51a3844b46dfd110b83
Lab <Lab@AIS-204LAB05>
2017-03-01 11:33:50 -0700

    git push

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -58,6 +58,11 @@ namespace SportsStore.WebUI.Controllers
             return PartialView(cart);
         }
 
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
+
        
     }
 }
