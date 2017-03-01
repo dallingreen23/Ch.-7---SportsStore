deaf4d3950ddafcca42200127b56ae3eec9c3d8e
Lab <Lab@AIS-204LAB05>
2017-03-01 10:44:41 -0700

    git push

--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -1,5 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Infrastructure_Binders;
+
 
 namespace SportsStore.WebUI
 {
@@ -9,6 +16,7 @@ namespace SportsStore.WebUI
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
     }
 }
